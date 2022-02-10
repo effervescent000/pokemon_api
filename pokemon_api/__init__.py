2eb6f390d241bcb4779e21691fb7bceec4bbcd50
effervescent000 <kislio01@gmail.com>
2022-02-10 09:02:52 -0500

    fixed issue with user model

--- pokemon_api/__init__.py
+++ pokemon_api/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 
 db = SQLAlchemy()
 ma = Marshmallow()
 cors = CORS()
+jwt = JWTManager()
 
 load_dotenv()
 
@@ -19,10 +21,15 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
     cors.init_app(app, supports_credentials=True)
+    jwt.init_app(app)
 
     with app.app_context():
         from .models import User, Note
 
         db.create_all()
+
+        from . import auth
+
+        app.register_blueprint(auth.bp)
 
         return app
--- pokemon_api/models.py
+++ pokemon_api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
         return f"User <{self.username}>"
